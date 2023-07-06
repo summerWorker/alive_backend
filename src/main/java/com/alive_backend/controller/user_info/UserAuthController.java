42650736fc3bd23c12d8bf371b9ce123704422a5
pp-hhh <yichen_X1@outlook.com>
2023-07-06 10:10:33 +0800

    finish code sending

--- src/main/java/com/alive_backend/controller/user_info/UserAuthController.java
+++ src/main/java/com/alive_backend/controller/user_info/UserAuthController.java
@@ -6,15 +6,19 @@ import com.alive_backend.entity.user_info.UserInfo;
 import com.alive_backend.service.health_data.MainRecordService;
 import com.alive_backend.service.user_info.UserAuthService;
 import com.alive_backend.service.user_info.UserInfoService;
+import com.alive_backend.serviceimpl.mail.MailService;
 import com.alive_backend.utils.constant.UserConstant;
 import com.alive_backend.utils.msg.Msg;
 import com.alive_backend.utils.msg.MsgUtil;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:3000")
@@ -26,143 +30,228 @@ public class UserAuthController {
     @Autowired
     private MainRecordService mainRecordService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private MailService mailService;
+
+    @PostMapping("/getCheckCode")
+    @ResponseBody
+    public Msg getCheckCode(String email){
+        String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
+        String message = "您的注册验证码为："+checkCode;
+        try {
+            mailService.sendSimpleMail(email, "注册验证码", message);
+        }catch (Exception e){
+            return MsgUtil.makeMsg(MsgUtil.ERROR,"发送失败");
+        }
+        redisTemplate.opsForValue().set(email, checkCode, 1, TimeUnit.MINUTES);
+
+        return MsgUtil.makeMsg(MsgUtil.SUCCESS,"发送成功");
+    }
+
+
+    /*
+    * register
+    */
+
+    @PostMapping("/register")
+    public Msg register(@RequestBody Map<String, Object> data){
+        Object name_obj = data.get(UserConstant.USERNAME);
+        Object password_obj = data.get(UserConstant.PASSWORD);
+        Object email_obj = data.get(UserConstant.EMAIL);
+        Object checkCode_obj = data.get(UserConstant.CHECK_CODE);
+
+        if (name_obj == null || password_obj == null || email_obj == null || checkCode_obj == null) {
+            return MsgUtil.makeMsg(MsgUtil.ERROR, "未传递用户名、密码、邮箱、验证码", null);
+        }
+
+        /*check username exists or not*/
+        String name = name_obj.toString();
+        UserAuth user = userAuthService.getUserAuthByName(name);
+        if(user != null){
+            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户名已被注册", null);
+        }
+
+        /*check email exists or not*/
+        String email = email_obj.toString();
+        user = userAuthService.getUserAuthByEmail(email);
+        if(user != null){
+            return MsgUtil.makeMsg(MsgUtil.ERROR, "邮箱已被注册", null);
+        }
+
+        /*check checkCode*/
+        String checkCode = checkCode_obj.toString();
+        String checkCodeInRedis = (String) redisTemplate.opsForValue().get(email);
+        redisTemplate.delete(email);
+        if(!checkCode.equals(checkCodeInRedis)){
+            return MsgUtil.makeMsg(MsgUtil.ERROR, "验证码错误", null);
+        }
+
+        /*add userAuth*/
+        String password = password_obj.toString();
+        user = new UserAuth();
+        user.setUsername(name);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setStatus(UserConstant.STATUS_ACTIVATE);
+        userAuthService.saveUserAuth(user);
+
+
+        int id = user.getUserId();
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(id);
+        userInfo.setNickname(name);
+        userInfoService.saveUserInfo(userInfo);
+
+        MainRecord mainRecord = new MainRecord();
+        mainRecord.setUserId(id);
+        Timestamp date = new Timestamp(System.currentTimeMillis());
+        mainRecord.setUpdateTime(date);
+        mainRecordService.updateMainRecord(mainRecord);
+
+        return MsgUtil.makeMsg(MsgUtil.SUCCESS, "注册成功", null);
+    }
+
+
     /**
      * 检查用户名是否重复
      * @param data: {username: 'xxx'}
      * @return Msg
      */
-    @PostMapping("/register/check_name")
-    public Msg checkName(@RequestBody Map<String,Object> data) {
-        /* 参数合法性检验 */
-        Object name_obj = data.get(UserConstant.USERNAME);
-        if (name_obj == null) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "未传递用户名{username: 'xxx'}", null);
-        }
-
-        String name = data.get(UserConstant.USERNAME).toString();
-        UserAuth user = userAuthService.getUserAuthByName(name);
-        if (user == null || user.getStatus() == UserConstant.STATUS_INACTIVATE) {
-            return MsgUtil.makeMsg(MsgUtil.SUCCESS, "用户名合法", null);
-        } else {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户名已被注册", null);
-        }
-    }
-
-    /**
-     * 注册第一步：发送邮箱验证码
-     * @param data: {username:'xxx',password:"xxx",email: 'xxx'}
-     * @return Msg
-     */
-    @PostMapping("/register/send_email_code")
-    public Msg register_step1(@RequestBody Map<String,Object> data) {
-        /* 参数合法性检验 */
-        Object name_obj = data.get(UserConstant.USERNAME);
-        Object password_obj = data.get(UserConstant.PASSWORD);
-        Object email_obj = data.get(UserConstant.EMAIL);
-        if (name_obj == null || password_obj == null || email_obj == null) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "传参错误：{\"username\": 'xxx', \"password\": 'xxx',\"email\": 'xxx'}", null);
-        }
-
-        /* 获取参数 */
-        String name = data.get(UserConstant.USERNAME).toString();
-        String password = data.get(UserConstant.PASSWORD).toString();
-        String email = data.get(UserConstant.EMAIL).toString();
-
-        /* 检验邮箱是否被注册 */
-        UserAuth user = userAuthService.getUserAuthByEmail(email);
-        if (user != null && user.getStatus() != UserConstant.STATUS_INACTIVATE) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "邮箱已被注册", null);
-        }
-
-        /* 解决发送过于频繁 */
-        if (user != null && user.getStatus() == UserConstant.STATUS_INACTIVATE) {
-            Timestamp date = new Timestamp(System.currentTimeMillis());
-            Timestamp last_date = user.getCodeUpdateTime();
-            if(last_date != null){
-                long time = date.getTime() - last_date.getTime();
-                if (time < 60000) {
-                    return MsgUtil.makeMsg(MsgUtil.ERROR, "发送过于频繁", null);
-                }
-            }
-        }
-
-        /* 保存用户 */
-        if(user == null)
-            user = new UserAuth();
-        user.setUsername(name); user.setPassword(password); user.setEmail(email);
-        user.setStatus(UserConstant.STATUS_INACTIVATE); // 未激活
-        // TODO: 生成验证码
-        user.setCheckCode("123456");
-        Timestamp date = new Timestamp(System.currentTimeMillis());
-        user.setCodeUpdateTime(date);
-        UserAuth update_user = userAuthService.saveUserAuth(user);
-        if (update_user == null) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户保存失败", null);
-        }
-
-        /* 发送邮箱验证码 */
-        //TODO
-
-        return MsgUtil.makeMsg(MsgUtil.SUCCESS, "邮件发送成功", null);
-    }
-
-    /**
-     * 注册第二步：激活用户
-     * @param data: {username:'xxx',check_code: 'xxx'}
-     * @return Msg
-     */
-    @PostMapping("/register")
-    public Msg register_step2(@RequestBody Map<String,Object> data) {
-        /* 参数合法性检验 */
-        Object name_obj = data.get(UserConstant.USERNAME);
-        Object check_code_obj = data.get(UserConstant.CHECK_CODE);
-        if (name_obj == null || check_code_obj == null) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "传参错误：{\"username\": 'xxx', \"check_code\": 'xxx'}", null);
-        }
-
-        /* 获取参数 */
-        String name = data.get(UserConstant.USERNAME).toString();
-        String check_code = data.get(UserConstant.CHECK_CODE).toString();
-
-        /* 检验邮箱验证码 */
-        UserAuth user = userAuthService.getUserAuthByName(name);
-        if (user == null) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户名不存在", null);
-        }
-        if (!user.getCheckCode().equals(check_code)) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "验证码错误", null);
-        }
-
-        /* 检验验证码是否过期: 5 min */
-        Timestamp date = new Timestamp(System.currentTimeMillis());
-        if (date.getTime() - user.getCodeUpdateTime().getTime() > 1000 * 60 * 5) {
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "验证码已过期", null);
-        }
-
-        /* 激活用户 */
-        user.setStatus(UserConstant.STATUS_ACTIVATE);
-        user.setCheckCode("");
-        int id = user.getUserId();
-
-        /* 初始化用户信息 */
-        UserInfo userInfo = new UserInfo();
-        userInfo.setUserId(id);
-        userInfo.setNickname(user.getUsername());
-
-        /* 初始化健康档案 */
-        MainRecord mainRecord = new MainRecord();
-        mainRecord.setUserId(id);
-        mainRecord.setUpdateTime(date);
-
-        UserAuth update_user = userAuthService.saveUserAuth(user);
-        UserInfo update_user_info = userInfoService.saveUserInfo(userInfo);
-        MainRecord update_main_record = mainRecordService.updateMainRecord(mainRecord);
-        if(update_user_info == null || update_main_record == null || update_user == null){
-            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户信息初始化失败", null);
-        }
-
-        return  MsgUtil.makeMsg(MsgUtil.SUCCESS, MsgUtil.RESIGN_SUCCESS_MSG, null);
-    }
+//    @PostMapping("/register/check_name")
+//    public Msg checkName(@RequestBody Map<String,Object> data) {
+//        /* 参数合法性检验 */
+//        Object name_obj = data.get(UserConstant.USERNAME);
+//        if (name_obj == null) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "未传递用户名{username: 'xxx'}", null);
+//        }
+//
+//        String name = data.get(UserConstant.USERNAME).toString();
+//        UserAuth user = userAuthService.getUserAuthByName(name);
+//        if (user == null || user.getStatus() == UserConstant.STATUS_INACTIVATE) {
+//            return MsgUtil.makeMsg(MsgUtil.SUCCESS, "用户名合法", null);
+//        } else {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户名已被注册", null);
+//        }
+//    }
+//
+//    /**
+//     * 注册第一步：发送邮箱验证码
+//     * @param data: {username:'xxx',password:"xxx",email: 'xxx'}
+//     * @return Msg
+//     */
+//    @PostMapping("/register/send_email_code")
+//    public Msg register_step1(@RequestBody Map<String,Object> data) {
+//        /* 参数合法性检验 */
+//        Object name_obj = data.get(UserConstant.USERNAME);
+//        Object password_obj = data.get(UserConstant.PASSWORD);
+//        Object email_obj = data.get(UserConstant.EMAIL);
+//        if (name_obj == null || password_obj == null || email_obj == null) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "传参错误：{\"username\": 'xxx', \"password\": 'xxx',\"email\": 'xxx'}", null);
+//        }
+//
+//        /* 获取参数 */
+//        String name = data.get(UserConstant.USERNAME).toString();
+//        String password = data.get(UserConstant.PASSWORD).toString();
+//        String email = data.get(UserConstant.EMAIL).toString();
+//
+//        /* 检验邮箱是否被注册 */
+//        UserAuth user = userAuthService.getUserAuthByEmail(email);
+//        if (user != null && user.getStatus() != UserConstant.STATUS_INACTIVATE) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "邮箱已被注册", null);
+//        }
+//
+//        /* 解决发送过于频繁 */
+//        if (user != null && user.getStatus() == UserConstant.STATUS_INACTIVATE) {
+//            Timestamp date = new Timestamp(System.currentTimeMillis());
+//            Timestamp last_date = user.getCodeUpdateTime();
+//            if(last_date != null){
+//                long time = date.getTime() - last_date.getTime();
+//                if (time < 60000) {
+//                    return MsgUtil.makeMsg(MsgUtil.ERROR, "发送过于频繁", null);
+//                }
+//            }
+//        }
+//
+//        /* 保存用户 */
+//        if(user == null)
+//            user = new UserAuth();
+//        user.setUsername(name); user.setPassword(password); user.setEmail(email);
+//        user.setStatus(UserConstant.STATUS_INACTIVATE); // 未激活
+//        // TODO: 生成验证码
+//        user.setCheckCode("123456");
+//        Timestamp date = new Timestamp(System.currentTimeMillis());
+//        user.setCodeUpdateTime(date);
+//        UserAuth update_user = userAuthService.saveUserAuth(user);
+//        if (update_user == null) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户保存失败", null);
+//        }
+//
+//        /* 发送邮箱验证码 */
+//        //TODO
+//
+//        return MsgUtil.makeMsg(MsgUtil.SUCCESS, "邮件发送成功", null);
+//    }
+//
+//    /**
+//     * 注册第二步：激活用户
+//     * @param data: {username:'xxx',check_code: 'xxx'}
+//     * @return Msg
+//     */
+//    @PostMapping("/register")
+//    public Msg register_step2(@RequestBody Map<String,Object> data) {
+//        /* 参数合法性检验 */
+//        Object name_obj = data.get(UserConstant.USERNAME);
+//        Object check_code_obj = data.get(UserConstant.CHECK_CODE);
+//        if (name_obj == null || check_code_obj == null) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "传参错误：{\"username\": 'xxx', \"check_code\": 'xxx'}", null);
+//        }
+//
+//        /* 获取参数 */
+//        String name = data.get(UserConstant.USERNAME).toString();
+//        String check_code = data.get(UserConstant.CHECK_CODE).toString();
+//
+//        /* 检验邮箱验证码 */
+//        UserAuth user = userAuthService.getUserAuthByName(name);
+//        if (user == null) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户名不存在", null);
+//        }
+//        if (!user.getCheckCode().equals(check_code)) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "验证码错误", null);
+//        }
+//
+//        /* 检验验证码是否过期: 5 min */
+//        Timestamp date = new Timestamp(System.currentTimeMillis());
+//        if (date.getTime() - user.getCodeUpdateTime().getTime() > 1000 * 60 * 5) {
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "验证码已过期", null);
+//        }
+//
+//        /* 激活用户 */
+//        user.setStatus(UserConstant.STATUS_ACTIVATE);
+//        user.setCheckCode("");
+//        int id = user.getUserId();
+//
+//        /* 初始化用户信息 */
+//        UserInfo userInfo = new UserInfo();
+//        userInfo.setUserId(id);
+//        userInfo.setNickname(user.getUsername());
+//
+//        /* 初始化健康档案 */
+//        MainRecord mainRecord = new MainRecord();
+//        mainRecord.setUserId(id);
+//        mainRecord.setUpdateTime(date);
+//
+//        UserAuth update_user = userAuthService.saveUserAuth(user);
+//        UserInfo update_user_info = userInfoService.saveUserInfo(userInfo);
+//        MainRecord update_main_record = mainRecordService.updateMainRecord(mainRecord);
+//        if(update_user_info == null || update_main_record == null || update_user == null){
+//            return MsgUtil.makeMsg(MsgUtil.ERROR, "用户信息初始化失败", null);
+//        }
+//
+//        return  MsgUtil.makeMsg(MsgUtil.SUCCESS, MsgUtil.RESIGN_SUCCESS_MSG, null);
+//    }
 
     @PostMapping("/login")
     public Msg Login(@RequestBody Map<String,Object> data) {
