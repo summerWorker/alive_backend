2365b72b792f16d2874116b1e936f619dff03378
GJFourier <592829411@qq.com>
2023-07-10 17:21:29 +0800

    test

--- src/main/java/com/alive_backend/controller/health_data/MainRecordController.java
+++ src/main/java/com/alive_backend/controller/health_data/MainRecordController.java
@@ -8,6 +8,7 @@ import com.alive_backend.utils.msg.MsgUtil;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class MainRecordController {
     @Autowired
     private MainRecordService mainRecordService;
--- src/main/java/com/alive_backend/controller/health_data/SleepController.java
+++ src/main/java/com/alive_backend/controller/health_data/SleepController.java
@@ -12,6 +12,7 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class SleepController {
     @Autowired
     private SleepDetailService sleepDetailService;
