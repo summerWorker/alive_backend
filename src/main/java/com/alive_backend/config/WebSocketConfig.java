63301982fe1fc0a4c56a4c52049f4debae23de7f
Doris-xm <dux.m.in@sjtu.edu.cn>
2023-07-12 15:37:00 +0800

    连接成功，实现echo功能

--- src/main/java/com/alive_backend/config/WebSocketConfig.java
+++ src/main/java/com/alive_backend/config/WebSocketConfig.java
@@ -2,41 +2,13 @@ package com.alive_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-import java.util.List;
-
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    //外部可订阅的socket数据服务(socket/topic为服务端广播消息的客户端订阅地址前缀，socket/user为客户端个性化消息的订阅地址前缀)
-    private static final String[] brokers = {
-            "/socket/topic","/socket/user"
-    };
-
-
-    //配置stomp端点，即客户端的握手连接机制
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket")   //注册Stomp端点，websocket客户端订阅或发布消息前，需通过此端点(路径)先连接(握手)
-                .setAllowedOriginPatterns("*")            //允许跨域访问
-                .withSockJS();                     //使用sockJS
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
-    // 消息代理(消息访问路径)
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker(brokers);// 配置客户端可订阅的消息前缀
-        registry.setApplicationDestinationPrefixes("/socket");//前端个性化订阅前，用stomp协议发送消息到服务端的地址前缀
-        registry.setUserDestinationPrefix("/socket/user");//推送消息到订阅用户的前缀地址(如前端订阅为"/user/message",则服务端send_to_user直接为"message")
-    }
+
 }
--- src/main/java/com/alive_backend/service/WebSocketService.java
+++
@@ -1,9 +0,0 @@
-package com.alive_backend.service;
-
-import java.util.Map;
-
-public interface WebSocketService {
-    void addUser(String username, String name);
-    void removeUser(String username) ;
-    Map<String, String> getNameMap();
-}
