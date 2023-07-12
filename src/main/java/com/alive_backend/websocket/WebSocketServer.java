d77e979f2094e85b67bc3b48c94dedd5eeb1f34f
Doris-xm <dux.m.in@sjtu.edu.cn>
2023-07-12 17:00:17 +0800

    实现体重目标提醒，每日更新

--- src/main/java/com/alive_backend/websocket/WebSocketServer.java
+++ src/main/java/com/alive_backend/websocket/WebSocketServer.java
@@ -1,28 +1,50 @@
 package com.alive_backend.websocket;
 
+import com.alive_backend.entity.goal.Goal;
+import com.alive_backend.service.goal.GoalService;
+import com.alive_backend.serviceimpl.goal.GoalServiceImpl;
+import com.alive_backend.utils.constant.GoalConstant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.concurrent.CopyOnWriteArraySet;
-@ServerEndpoint(value = "/websocket")
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+
+//@ServerEndpoint(value = "/websocket/{token}") //响应路径为 /ws/{token} 的连接请求
+@ServerEndpoint(value = "/websocket/{userId}") //响应路径为 /ws/{token} 的连接请求
 @Component
 public class WebSocketServer {
+    /* fix Component中Autowired注入为null问题 */
+    private static GoalService goalService;
+    @Autowired
+    public void setGoalService(GoalService goalService) {
+        WebSocketServer.goalService = goalService;
+    }
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
+
+    private Map<Integer, ScheduledExecutorService> schedulerMap = new ConcurrentHashMap<>();
 
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
+    private Integer userId;
 
     /**
      * 连接建立成功调用的方法*/
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session,@PathParam("userId") Integer uid) {
         this.session = session;
+        this.userId = uid;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
@@ -31,6 +53,36 @@ public class WebSocketServer {
         } catch (IOException e) {
             System.out.println("IO异常");
         }
+        // 创建一个定时任务调度器
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        schedulerMap.put(uid, scheduler);
+
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                sendMessage("你好"+uid);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Goal weight_goal = goalService.getGoalByGoalName(uid, GoalConstant.WEIGHT_GOAL);
+            System.out.println(weight_goal);
+            if(weight_goal != null) {
+                Date weight_goal_date = weight_goal.getGoalDdl();
+                long weight_goal_time = weight_goal_date.getTime();
+                long now_time = System.currentTimeMillis();
+                long time = weight_goal_time - now_time;
+                if(time > 0) {
+
+                    try {
+                        int left_days = (int) (time / (1000 * 60 * 60 * 24));
+                        sendMessage("设置了体重目标:"+ weight_goal.getGoalKey1() +" kg，还有"+left_days+"天截止");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }, 0, 1, TimeUnit.DAYS);
     }
 
     /**
@@ -41,6 +93,12 @@ public class WebSocketServer {
         webSocketSet.remove(this);  //从set中删除
         subOnlineCount();           //在线数减1
         System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+
+        // 关闭并移除定时任务调度器
+        ScheduledExecutorService scheduler = schedulerMap.remove(this.userId);
+        if (scheduler != null) {
+            scheduler.shutdown();
+        }
     }
 
     /**
@@ -60,6 +118,7 @@ public class WebSocketServer {
             }
         }
     }
+
 
     /**
      * 发生错误时调用
